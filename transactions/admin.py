3c86e94c175f4fb9b44598c8339659eb303f83b9
humans-2025 <humansuz2022@gmail.com>
2025-09-19 21:40:22 +0500

    admin.py

---
+++ transactions/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Transaction, Deposit, Withdraw
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("user", "type", "amount", "status", "created_at")
+    list_filter = ("type", "status")
+    search_fields = ("user__username", "user__phone")
+
+
+@admin.register(Deposit)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("user__username", "user__phone")
+
+
+@admin.register(Withdraw)
+class WithdrawAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("user__username", "user__phone")
